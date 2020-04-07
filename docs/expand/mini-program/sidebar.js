90deef97a03f43aedb26af2bfd78ce166b167d22
zhongtao <1414241730@qq.com>
2020-04-07 19:45:38 +0800

    wx

--- docs/expand/mini-program/sidebar.js
+++ docs/expand/mini-program/sidebar.js
@@ -5,7 +5,8 @@ module.exports = [
       path: '/expand/mini-program/',
       children: [
           'notes/小程序介绍',
-          'notes/小程序的MVVM框架'
+          'notes/小程序的MVVM框架',
+          'notes/掌握project和sitemap',
       ]
   }
 ]
