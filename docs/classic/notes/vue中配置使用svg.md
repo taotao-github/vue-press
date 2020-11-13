d5d912ee8502edbe0aa7344613b740df12dc9553
zt_git_pro <zt_git_pro123@xy.com>
2020-11-13 16:01:15 +0800

    vue 配置svg

---
+++ docs/classic/notes/vue中配置使用svg.md
@@ -0,0 +1,8 @@
+# vue中配置使用svg
+
+1. 安装svg-sprite-loader
+```sh
+cnpm install --save svg-sprite-loader
+```
+1. 在webpack中配置svg的处理
+2. 在全局组件目录中封装svg组件
--- docs/classic/sidebar.js
+++ docs/classic/sidebar.js
@@ -11,6 +11,14 @@ module.exports = [
                     { title: "min()、max()和clamp()介绍与使用", path: "notes/one" }
                 ],
             },
+
+            {
+                title: "vue-cli3项目配置",
+                collapsable: false,
+                children: [
+                    { title: "vue中配置使用svg", path: "notes/vue中配置使用svg" }
+                ],
+            }
         ]
     }
 ]
